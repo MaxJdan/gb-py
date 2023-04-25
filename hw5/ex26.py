fb3bf313448e7e8fbf0fc648a791c8b10a002f10
Max Jdanov <maxzhdanov@list.ru>
2023-04-25 16:15:36 +0300

    hw5

--- hw5/ex26.py
+++ hw5/ex26.py
@@ -1,8 +1,10 @@
 a = int(input("Введите число: "))
 b = int(input("В какую степень возвести число: "))
+
 def deg(a, b):
   if b == 1:
     return a
   if b != 1:
     return a * deg(a, b - 1)
+    
 print(deg(a, b))
